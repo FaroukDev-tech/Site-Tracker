5d3d776e7a93281e5e4b0c4a54dc5ae73b0a61c0
Far9999 <sadikalhanssah@gmail.com>
2023-10-05 15:43:55 +0000

    fix errors and bugs

--- index.js
+++ index.js
@@ -4,30 +4,46 @@ const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const clearBtn = document.getElementById("clear-btn")
 const outputBtn = document.getElementById("output-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const sitesFromLocalStorage = JSON.parse(localStorage.getItem("Sites"))
+
+if (sitesFromLocalStorage) {
+    mySites = sitesFromLocalStorage
+    renderSites() 
+} else {
+    ulEl.innerHTML = `<p style="font-style:italic">Nothing saved yet.</p>`
+}
 
 inputBtn.addEventListener("click", function (){
+    if (mySites.length===0)
+        ulEl.innerHTML = ""
     mySites.push(inputEl.value); 
     ulEl.innerHTML += `<li><a href=${inputEl.value} target="_blank">${inputEl.value}</a>`
-    inputEl.value = null;
+    inputEl.value = "";
     localStorage.setItem("Sites", JSON.stringify(mySites))
 })
 
 outputBtn.addEventListener("click", function () {
-    renderSites()
-})
+    if (mySites.length===0)
+        ulEl.innerHTML = `<p style="font-style:italic">Nothing saved yet.</p>`
+    else
+        renderSites()
+}) 
 
 clearBtn.addEventListener("click", function (){
     ulEl.innerHTML = ""
+})
+
+deleteBtn.addEventListener("dblclick", function (){
     localStorage.clear()
+    ulEl.innerHTML = ""
+    mySites = []
 })
 
 function renderSites(){
-    let sites = localStorage.getItem("Sites")
-    sites = JSON.parse(sites)
-
     ulEl.innerHTML = "";
-    for (let i=0; i < sites.length; i++){
-        ulEl.innerHTML += `<li><a href=${sites[i]} target="_blank">${sites[i]}</a>`
+    for (let i=0; i < mySites.length; i++){
+        ulEl.innerHTML += `<li><a href=${mySites[i]} target="_blank">${mySites[i]}</a>`
     }
 }
 
